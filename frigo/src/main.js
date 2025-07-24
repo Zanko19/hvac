a3351fd4c7de3654f3bf24b5c2b4ce30bc4e5a70
Guillaume <guizanko@hotmail.fr>
2025-07-24 11:12:28 +0200

    later

--- frigo/src/main.js
+++ frigo/src/main.js
@@ -1,2 +1,11 @@
+import { createAbout } from './components/about.js';
+import { createContact } from './components/contact.js';
+
+const app = document.querySelector('#app');
+app.innerHTML = `
+  ${createAbout()}
+  ${createContact()}
+`;
+
 
 import './index.css'
